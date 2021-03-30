b2fcb147b4ae7ddb5f6d49a4e7f1b2fa8cb5832a
Chris Mailer <christophermailer@icloud.com>
2021-03-30 15:51:59 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
 from simulator import Simulator
 from controllers.kinematic import Controller, tripod_gait
 
-controller = Controller(tripod_gait, body_height=0.15, velocity=0.3, crab_angle=-1.57)
-simulator = Simulator(controller, follow=True, visualiser=True, collision_fatal=False, failed_legs=[1])
+controller = Controller(tripod_gait, body_height=0.15, velocity=0.4, crab_angle=-1.57)
+simulator = Simulator(controller, follow=True, visualiser=True, collision_fatal=False, failed_legs=[])
 
 while True:
 	simulator.step()
