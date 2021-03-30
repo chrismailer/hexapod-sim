f03e5a77e56a6f7d31a2bcdc46a2e5e011afa5a5
Chris Mailer <christophermailer@icloud.com>
2021-03-30 10:27:40 +0200

    Minor changes

--- simulator.py
+++ simulator.py
@@ -11,7 +11,8 @@ class Simulator:
 
 	Note
 	----
-	This class is configured to automatically start a PyBullet instance on an available CPU core
+	This class is configured to automatically start a PyBullet instance on an available CPU core.
+	Each initialised class will use a separate CPU aiding parallelisation
 
     Attributes
     ----------    	
@@ -29,9 +30,10 @@ class Simulator:
     	A list of joint numbers which should simulated as unpowered.
 
     """
-	def __init__(self, controller, urdf='/urdf/hexapod.urdf', visualiser=False, follow=True, collision_fatal=True, failed_legs=[], camera_position=[0, 0, 0], camera_distance=0.7, camera_yaw=20, camera_pitch=-30):
+	def __init__(self, controller, urdf='/urdf/hexapod_simplified.urdf', visualiser=False, follow=True, collision_fatal=True, failed_legs=[], camera_position=[0, 0, 0], camera_distance=0.7, camera_yaw=20, camera_pitch=-30):
 		self.t = 0 #: float: Current time of the simulator
-		self.dt = 1/240  #: float: Timestep of simulator. Default is 1/240s for PyBullet.
+		self.dt = 1.0/240.0  #: float: Timestep of simulator. Default is 1/240s for PyBullet.
+		self.n_step = 0
 		self.gravity = -9.81 #: float: Magnitude of gravity vector in the positive z direction
 		self.foot_friction = 0.7
 		self.controller = controller
@@ -60,7 +62,7 @@ class Simulator:
 			self.client.resetDebugVisualizerCamera(cameraDistance=self.camera_distance, cameraYaw=self.camera_yaw, cameraPitch=self.camera_pitch, cameraTargetPosition=self.camera_position)
 			self.client.configureDebugVisualizer(p.COV_ENABLE_GUI, False) # somestimes useful to turn on
 			self.client.configureDebugVisualizer(p.COV_ENABLE_RENDERING, True)
-			self.client.configureDebugVisualizer(p.COV_ENABLE_SHADOWS, False, shadowMapResolution=16384) # sometimes useful to turn on
+			self.client.configureDebugVisualizer(p.COV_ENABLE_SHADOWS, False, shadowMapResolution=8192) # for nice rendering
 			self.client.configureDebugVisualizer(p.COV_ENABLE_WIREFRAME, False) # somestimes useful to turn on
 			self.client.configureDebugVisualizer(p.COV_ENABLE_RGB_BUFFER_PREVIEW, False)
 			self.client.configureDebugVisualizer(p.COV_ENABLE_DEPTH_BUFFER_PREVIEW, False)
@@ -72,11 +74,8 @@ class Simulator:
 		self.client.setGravity(0, 0, self.gravity)
 		self.client.setRealTimeSimulation(False) # simulation needs to be explicitly stepped
 
-		# profiling
-		# self.logId = self.client.startStateLogging(p.STATE_LOGGING_VIDEO_MP4, "video.mp4")
-
 		# Add ground plane and set lateral friction coefficient
-		self.groundId = self.client.loadURDF("plane.urdf") #: int: Body ID of ground
+		self.groundId = self.client.loadURDF('plane.urdf') #: int: Body ID of ground
 		self.client.changeDynamics(self.groundId, -1, lateralFriction=self.foot_friction)
 		# Add hexapod URDF
 		position = [0, 0, self.controller.body_height]
@@ -129,6 +128,7 @@ class Simulator:
 		for link_index in femur_links:
 			self.client.setCollisionFilterPair(self.hexId, self.hexId, linkIndexA=-1, linkIndexB=link_index, enableCollision=0)
 
+
 	# Fetches and stores the joint index and joint information in the expected order
 	def __get_joints(self, robotId):
 		# A lot of the joint information in the URDF is not used in setJointMotorControl and needs to be manually applied
@@ -141,7 +141,7 @@ class Simulator:
 				# [ joint_index, lower_limit, upper_limit, max_torque, max_velocity ]
 				joints[index] = [info[0], info[8], info[9], info[10], info[11]]
 			except ValueError:
-				print("Unexpected joint name in URDF")
+				print('Unexpected joint name in URDF')
 		return joints
 
 
@@ -171,7 +171,7 @@ class Simulator:
 			# self.pipeline.wait()
 			self.client.disconnect()
 		except p.error as e:
-			print("Termination of simulation failed:", e)
+			print('Termination of simulation failed:', e)
 
 
 	def step(self):
@@ -185,29 +185,24 @@ class Simulator:
 		start_time = time.perf_counter()
 		# using setJointMotorControl2 (slightly slower but allows setting of max velocity)
 		joint_angles = self.controller.joint_angles(t=self.t)
-		joint_speeds = self.controller.joint_speeds(t=self.t)
 
-		for index, joint in enumerate(self.joints):
-			joint_index, lower_limit, upper_limit, max_torque, max_speed = joint
+		for index, joint_properties in enumerate(self.joints):
+			joint_index, lower_limit, upper_limit, max_torque, max_speed = joint_properties
 			
 			# skip if joint is not present of if failed
 			if (joint_index is None) or (index in self.locked_joints) or (index in self.failed_joints): continue
 
 			joint_angle = joint_angles[index]
-			joint_speed = joint_speeds[index]
 			
 			# ensure controller does not attempt to exceed joint limits
 			joint_angle = min(max(lower_limit, joint_angle), upper_limit)
-			joint_speed = min(max(-max_speed, joint_speed), +max_speed)
 
 			# max velocity in URDF isn't used and needs to be assigned
-			# self.client.setJointMotorControl2(self.hexId, joint_index, p.POSITION_CONTROL, targetPosition=joint_angle, targetVelocity=joint_speed, force=max_torque, maxVelocity=max_speed)
 			self.client.setJointMotorControl2(self.hexId, joint_index, p.POSITION_CONTROL, targetPosition=joint_angle, force=max_torque, maxVelocity=max_speed)
-
 
 		if self.collision_fatal:
 			if self.__link_collision() or self.__ground_collision():
-				raise RuntimeError("Link collision during simulation")
+				raise RuntimeError('Link collision during simulation')
 
 		end_time = time.perf_counter()
 		# follow robot with camera
@@ -216,21 +211,13 @@ class Simulator:
 				self.client.resetDebugVisualizerCamera(cameraDistance=self.camera_distance, cameraYaw=self.camera_yaw, cameraPitch=self.camera_pitch, cameraTargetPosition=self.base_pos())
 			time.sleep(max(self.dt - end_time + start_time, 0))
 
-		# if ((round(self.t / self.dt) % 10) == 0):
-		# 	width = int(1920*1)
-		# 	height = int(1080*1)
-		# 	projection_matrix = self.client.computeProjectionMatrixFOV(fov=60, aspect=width/height, nearVal=0.01, farVal=100.0)
-		# 	view_matrix = self.client.computeViewMatrixFromYawPitchRoll(cameraTargetPosition=self.camera_position, distance=self.camera_distance, yaw=self.camera_yaw, pitch=self.camera_pitch, roll=0, upAxisIndex=2)
-
-		# 	width, height, rgbPixels, depthPixels, segmentationMaskBuffer = self.client.getCameraImage(width, height, viewMatrix=view_matrix, projectionMatrix=projection_matrix, shadow=1, lightDirection=[1, 1, 1], renderer=p.ER_BULLET_HARDWARE_OPENGL)
-		# 	image = Image.fromarray(rgbPixels, 'RGBA')
-		# 	image = image.convert("RGB")
-		# 	image.save(self.pipeline.stdin, 'JPEG')
+		# controller feedback running at 10Hz
+		if ((self.n_step % 24) == 0):
+			self.controller.IMU_feedback(self.base_orientation())
 
 		self.client.stepSimulation()
-
+		self.n_step += 1
 		self.t += self.dt
-
 	
 	def supporting_legs(self):
 		"""Determines the supporting legs for the hexapod
@@ -258,7 +245,7 @@ class Simulator:
 		return supporting_legs
 
 
-	# Check for collision between links in robot
+	# Returns true for collision between links in robot
 	def __link_collision(self):
 		# Get contact points between hex links
 		# Collision between child and parent in URDF is disabled by default so shouldn't need to ignore this
@@ -267,6 +254,7 @@ class Simulator:
 		return contact_points.size > 0
 
 
+	# Returns true for collisions between the robot and the ground
 	def __ground_collision(self):
 		tibia_links = self.links[:, 2]
 		# Get contact points between hex and ground for the last stepSimulation call
@@ -281,6 +269,11 @@ class Simulator:
 
 		# returns a boolean array of whether tibia is in contact with the ground or not
 		return contact_links.size > 0
+
+
+	def base_orientation(self):
+		quaternion = self.client.getBasePositionAndOrientation(self.hexId)[1]
+		return p.getEulerFromQuaternion(quaternion)
 
 
 	# returns the base position without the orientation
@@ -304,7 +297,7 @@ def evaulate_gait(leg_params, body_height=0.14, velocity=0.3, duration=5.0, visu
 	controller = Controller(leg_params, body_height=body_height, velocity=velocity, crab_angle=-np.pi/6)
 	# initialise simulator
 	simulator = Simulator(controller, follow=True, visualiser=visualiser, collision_fatal=collisions, failed_legs=failed_legs)
-	# initialise reward and descriptor
+	# initialise fitness and descriptor
 	contact_sequence = np.full((6, 0), False)
 	# simulator returns error if collision occurs
 	for t in np.arange(0, duration, step=simulator.dt):
@@ -312,10 +305,10 @@ def evaulate_gait(leg_params, body_height=0.14, velocity=0.3, duration=5.0, visu
 			simulator.step()
 		except RuntimeError as error:
 			print(error)
-			reward = 0
+			fitness = 0
 			break
 		contact_sequence = np.append(contact_sequence, simulator.supporting_legs().reshape(-1,1), axis=1)
-	reward = simulator.base_pos()[0]
+	fitness = simulator.base_pos()[0]
 	# summarise descriptor
 	descriptor = np.sum(contact_sequence, axis=1) / np.size(contact_sequence, axis=1)
 	# plot footfall diagram
@@ -323,7 +316,7 @@ def evaulate_gait(leg_params, body_height=0.14, velocity=0.3, duration=5.0, visu
 
 	simulator.terminate()
 
-	return reward, descriptor
+	return fitness, descriptor
 
 
 
